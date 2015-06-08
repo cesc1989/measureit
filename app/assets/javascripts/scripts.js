aedb855e4b8d1d26a92ae921e543ebf74fff794b
Francisco Quintero Linux Mint <frajaquico@gmail.com>
2015-06-08 00:07:58 -0500

    agregar tarea reciente luego de finalizada

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -6,13 +6,17 @@ $(document).ready(function(){
 	var startTime;
 	var stopTime;
 
+	var timer;
+
+	var keepTaskDescription;
+
 	var minutesLabel = document.getElementById("minutes");
     var secondsLabel = document.getElementById("seconds");
     var totalSeconds = 0;
 
 	$("#startTimer").click(function(){
 
-        setInterval(setTime, 1000);
+        timer = setInterval(setTime, 1000);
 
 		var createTaskUrl = "/start";
 		var taskDescription;
@@ -32,9 +36,6 @@ $(document).ready(function(){
 
 		startTime = getTime();
 
-		//console.log(data);
-		//console.log(fulltime);
-
 		$.ajax({
 			type: "POST",
 			url: createTaskUrl,
@@ -42,7 +43,8 @@ $(document).ready(function(){
 			dataType: "JSON",
 			success: function(data){
 				console.log("Listo el pollo");
-				console.log(data);
+				//console.log("Información de la tarea:");
+				//console.log(data);
 				getTaskId(data);
 			},
 			error: function(jqXHR, textStatus, errorMessage){
@@ -75,22 +77,50 @@ $(document).ready(function(){
 			dataType: "JSON",
 			success: function(data){
 				console.log("Listo el pollo");
-				console.log(data);
+				//console.log(data);
+				drawTask(keepTaskDescription);
+				getElapsedTime(data);
 			},
 			error: function(jqXHR, textStatus, errorMessage){
 				console.log("Hubo errores en el proceso: "+errorMessage);
 			}
-
 		});
 
+		stopTimer();
+
 	});
+
+	var getElapsedTime = function(data){
+		var st, et, tt;
+		for (key in data){
+			st = data[key].start_time;
+			et = data[key].end_time;
+		}
+
+		/*st = new Date(st).toString();
+		et = new Date(et).toString();*/
+
+		/*st = st.split("T").join(" ");
+		st = st.split(".000Z").join("");
+
+		et = et.split("T").join(" ");
+		et = et.split(".000Z").join("");*/
+
+		console.log(st+" "+et);
+
+		tt = new Date(et.getTime() - st.getTime());
+		console.log(tt);
+	}
+
+	function drawTask(data){
+		$(".tasksList").prepend("<p>"+data+"</p>");
+	}
 
 	function getTaskId(data){
 		for(var key in data){
 			taskId = data[key].id;
-			//console.log(data[key].id);
+			keepTaskDescription = data[key].description;
 		}
-		//console.log("Task id es: "+taskId);
 	};
 
 	var getTime = function(){
@@ -111,6 +141,13 @@ $(document).ready(function(){
         ++totalSeconds;
         secondsLabel.innerHTML = pad(totalSeconds%60);
         minutesLabel.innerHTML = pad(parseInt(totalSeconds/60));
+    }
+
+    function stopTimer(){
+    	clearInterval(timer);
+    	secondsLabel.innerHTML = "00";
+    	minutesLabel.innerHTML = "00";
+    	$("#timerInput").val("");
     }
 
     function pad(val){
