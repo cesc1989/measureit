c8e209a7e8bd902aceb71a7a3d9ca5ce1da51fc3
Francisco Quintero Linux Mint Thinkpad <frajaquico@yahoo.es>
2018-07-06 20:43:35 -0500

    Do not use Sentry yet

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,4 @@
-Raven.configure do |config|
-  config.dsn = Rails.application.secrets.sentry_dsn
-  config.environments = %w[ production ]
-end
+# Raven.configure do |config|
+#   config.dsn = Rails.application.secrets.sentry_dsn
+#   config.environments = %w[ production ]
+# end
